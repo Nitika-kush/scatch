69823303d2b68f7d9b3d520be62343b8b10669ef
Nitika-kush <nikushwaha@bestpeers.com>
2025-05-08 15:52:38 +0530

    routers set-up

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const cookieParser = require('cookie-parser');
 const express=require('express');
 const app=express();
 const path=require('path');
+const db=require('./config/mongoose-connection')
+const ownersRouter=require('./routes/ownersRouter')
+const productsRouter=require('./routes/productsRouter')
+const usersRouter=require('./routes/usersRouter')
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -9,8 +14,8 @@ app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 app.use(cookieParser());
 
-app.get('/',(req,res)=>{
-res.send('Hello')
-})
+app.use('/owners',ownersRouter);
+app.use('/products',productsRouter);
+app.use('/users',usersRouter);
 
 app.listen(8000);
--- models/product-model.js
+++ models/product-model.js
@@ -1,6 +1,5 @@
 const mongoose=require('mongoose');
 
-
 const productSchema=mongoose.Schema({
   name:String,
   image:String,
